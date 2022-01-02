07e9bccbc18f689da505340e14eaf713a4bcb5e8
Alex <nypels.alexandre@outlook.com>
2022-01-02 00:54:18 +0000

    Adding babel

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,20 @@ module.exports = {
     },
     plugins: [
         new webpack.ProvidePlugin({
-          $: 'jquery',
-          jQuery: 'jquery',
+            $: 'jquery',
+            jQuery: 'jquery',
         }),
     ],
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
 };
